9da8f64e548ed0cb002f1533df56500ad7a610a3
elheck <aron.heck@gmx.de>
2022-09-17 10:12:09 +0000

    feat: one problem solved other one appeared

--- src/dht.rs
+++ src/dht.rs
@@ -9,21 +9,23 @@ pub struct DHTs{
 }
 
 trait InputOutputPin: InputPin<Error = EspError> + OutputPin<Error = EspError>{}
+impl<T: InputPin<Error = EspError> + OutputPin<Error = EspError>> InputOutputPin for T {}
 
 impl DHTs{
     pub fn new(pins: Vec<(u8, &str)>) -> (){
         for (pin, name) in pins.iter(){
-            let gpio = Self::get_gpio(pin);
+            let gpio = Self::get_gpio(pin).unwrap();
+            let dht = Dht11::new(*gpio);
         }
     }
 
-    fn get_gpio(pin: &u8) -> Result<Box<dyn InputOutputPin>, &'static str>{
+    fn get_gpio(pin: &u8) -> Result<&dyn InputOutputPin, &'static str>{
         match pin {
-            33 => Ok(Box::new(Peripherals::take().unwrap().pins.gpio33.into_input_output().unwrap())),
-            32 => Ok(Box::new(Peripherals::take().unwrap().pins.gpio32.into_input_output().unwrap())),
-            27 => Ok(Box::new(Peripherals::take().unwrap().pins.gpio27.into_input_output().unwrap())),
-            26 => Ok(Box::new(Peripherals::take().unwrap().pins.gpio26.into_input_output().unwrap())),
-            25 => Ok(Box::new(Peripherals::take().unwrap().pins.gpio25.into_input_output().unwrap())),
+            33 => Ok(&Peripherals::take().unwrap().pins.gpio33.into_input_output().unwrap()),
+            32 => Ok(&Peripherals::take().unwrap().pins.gpio32.into_input_output().unwrap()),
+            27 => Ok(&Peripherals::take().unwrap().pins.gpio27.into_input_output().unwrap()),
+            26 => Ok(&Peripherals::take().unwrap().pins.gpio26.into_input_output().unwrap()),
+            25 => Ok(&Peripherals::take().unwrap().pins.gpio25.into_input_output().unwrap()),
             _ => Err("Pin not configurable for dht")
         }
     }
