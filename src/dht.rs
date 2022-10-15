4c5510323d6733f8d8922c2c7b6bfc69a4a03799
elheck <aron.heck@gmx.de>
2022-10-15 20:02:22 +0000

    feat:finally pin mapping like I wanted it

--- src/dht.rs
+++ src/dht.rs
@@ -1,5 +1,5 @@
-use dht11::Dht11;
-use esp_idf_hal::prelude::Peripherals;
+
+use esp_idf_hal::{prelude::Peripherals, gpio::{InputOutput, GpioPin}};
 use std::any::Any;
 
 pub struct DHTs{
@@ -10,39 +10,20 @@ impl DHTs{
     pub fn new(pins: Vec<(u8, &str)>) -> Self{
         let mut instance = Self{dhts: Vec::new()};
         for (pin, name) in pins.iter(){
-            instance.add_dht_for_pin(*pin, String::from(*name));
+            let gpio = DHTs::get_gpio(pin).unwrap();
+            instance.dhts.push((name.to_string(), Box::new(gpio)));
         }
         instance
     }
 
-    fn add_dht_for_pin(&mut self, pin: u8, name: String) -> (){
+    fn get_gpio(pin: &u8) -> Result<GpioPin<InputOutput>, &'static str>{
         match pin {
-            33 => {
-                let chosen_pin = Peripherals::take().unwrap().pins.gpio33.into_input_output().unwrap();
-                let dht = Dht11::new(chosen_pin);
-                self.dhts.push((name, Box::new(dht)))
-            },
-            32 => {
-                let chosen_pin = Peripherals::take().unwrap().pins.gpio32.into_input_output().unwrap();
-                let dht = Dht11::new(chosen_pin);
-                self.dhts.push((name, Box::new(dht)))
-            },
-            27 => {
-                let chosen_pin = Peripherals::take().unwrap().pins.gpio27.into_input_output().unwrap();
-                let dht = Dht11::new(chosen_pin);
-                self.dhts.push((name, Box::new(dht)))
-            },
-            26 => {
-                let chosen_pin = Peripherals::take().unwrap().pins.gpio26.into_input_output().unwrap();
-                let dht = Dht11::new(chosen_pin);
-                self.dhts.push((name, Box::new(dht)))
-            },
-            25 => {
-                let chosen_pin = Peripherals::take().unwrap().pins.gpio25.into_input_output().unwrap();
-                let dht = Dht11::new(chosen_pin);
-                self.dhts.push((name, Box::new(dht)))
-            },
-            _ => panic!("This pin cannot be used")
+            33 => Ok(Peripherals::take().unwrap().pins.gpio33.into_input_output().unwrap().degrade()),
+            32 => Ok(Peripherals::take().unwrap().pins.gpio32.into_input_output().unwrap().degrade()),
+            27 => Ok(Peripherals::take().unwrap().pins.gpio27.into_input_output().unwrap().degrade()),
+            26 => Ok(Peripherals::take().unwrap().pins.gpio26.into_input_output().unwrap().degrade()),
+            25 => Ok(Peripherals::take().unwrap().pins.gpio25.into_input_output().unwrap().degrade()),
+            _ => Err("Pin not configurable for dht")
         }
     }
 }
