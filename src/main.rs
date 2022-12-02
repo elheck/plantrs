5cf6bc4bdbc4c7845d50800c1484b25db638a748
elheck <aron.heck@gmx.de>
2022-12-02 22:12:06 +0100

    feat: openocd debugging works

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,68 @@
-use std::thread;
-use std::time;
-use esp_idf_svc::wifi::EspWifi;
-use esp_idf_hal::peripherals::Peripherals;
+//! Blinks the LED on a Pico board
+//!
+//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
+#![no_std]
+#![no_main]
 
+use bsp::entry;
+use defmt::*;
+use defmt_rtt as _;
+use embedded_hal::digital::v2::OutputPin;
+use panic_probe as _;
 
-// This is a config file that defines the following private parameter constants
-mod settings;
-mod dht;
-use crate::dht::DHTs;
+// Provide an alias for our BSP so we can switch targets quickly.
+// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
+use rp_pico as bsp;
+// use sparkfun_pro_micro_rp2040 as bsp;
 
-mod pump;
-use crate::pump::Pump;
+use bsp::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    pac,
+    sio::Sio,
+    watchdog::Watchdog,
+};
 
-mod wifi;
-use crate::wifi::connect_wifi;
+#[entry]
+fn main() -> ! {
+    info!("Program start");
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
 
-mod pin_switch;
-use crate::pin_switch::Switch;
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
 
-const TEST_NAME: &str = "Test";
-const TEST_DHT: (u8, &str) = (33, TEST_NAME);
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-fn main() {
-    let _wifi:EspWifi = connect_wifi().unwrap();
+    let pins = bsp::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
 
-    esp_idf_svc::log::EspLogger::initialize_default();
-    let pin2 = Peripherals::take().unwrap().pins.gpio2.into_output().unwrap();
-    let pin17 = Peripherals::take().unwrap().pins.gpio17.into_output().unwrap();
-    let mut pump = Pump::new(Switch::new(pin2), Switch::new(pin17));
-    let mut dhts = DHTs::new(vec![TEST_DHT]);
+    let mut led_pin = pins.led.into_push_pull_output();
 
-    loop{
-        pump.turn_on_blocking(time::Duration::from_millis(1000));
-        thread::sleep(time::Duration::from_millis(1000));
-        dhts.get_measurements();
-        dhts.get_measurement_for(TEST_NAME).unwrap();
+    loop {
+        info!("on!");
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        info!("off!");
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
     }
 }
+
+// End of file
