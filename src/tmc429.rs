dc33417e6424af13ef525af8b477137582e6cc50
elheck <aron.heck@gmx.de>
2023-02-24 22:08:09 +0100

    feat: added FullDuplex

--- src/tmc429.rs
+++ src/tmc429.rs
@@ -1,9 +1,13 @@
 
 use embedded_hal::digital::v2::OutputPin;
 use rp_pico::hal::{Spi, spi::{Enabled, SpiDevice}};
+use embedded_hal::spi::FullDuplex;
+
+const CLOCK_FREQUENCY_MHZ_MAX: u8 = 32;
 
 pub enum Error{
     ChipSelectError,
+    ClockFrequencyError,
 }
 
 #[derive(PartialEq, Eq)]
@@ -32,27 +36,26 @@ impl Default for MotorConfig{
     }
 }
 
-
-pub struct TMC429<D, CS, const DS: u8> 
+pub struct TMC429<CS, T> 
 where
-    D: SpiDevice,
     CS: OutputPin,
-    CS::Error: core::fmt::Debug
-
+    CS::Error: core::fmt::Debug,
+    T: FullDuplex<u8>
 {
-    spi: Spi<Enabled, D, DS>,
+    spi: T,
     chip_select: CS,
-    motors: [MotorConfig; 3]
+    motors: [MotorConfig; 3],
+    clock_frequency: u8
     
 }
 
-impl <D, CS, const DS: u8> TMC429<D, CS, DS> 
+impl <CS, T> TMC429<CS, T>
 where
-    D: SpiDevice,
     CS: OutputPin,
-    CS::Error: core::fmt::Debug
+    CS::Error: core::fmt::Debug,
+    T: FullDuplex<u8>
 {
-    pub fn new(chip_select_pin: CS, spi: Spi<Enabled, D, DS>) ->Self{
+    pub fn new(chip_select_pin: CS, spi: T) ->Self{
         let motors = [
             MotorConfig{
                 ..Default::default()
@@ -70,7 +73,8 @@ where
         TMC429{
             spi, 
             chip_select: chip_select_pin, 
-            motors
+            motors,
+            clock_frequency: CLOCK_FREQUENCY_MHZ_MAX
         }
     }
     
@@ -84,14 +88,25 @@ where
         Err("Couldn't find motor")
     }
 
+    pub fn init(&mut self, clock_fequency_in_mhz: u8) -> Result<(), Error>{
+        self.set_clock_frequency(clock_fequency_in_mhz)?;
+        Ok(())
+    }   
+
+    fn set_clock_frequency(&mut self, clock_frequency_in_mhz: u8) -> Result<(), Error>{
+        if clock_frequency_in_mhz > CLOCK_FREQUENCY_MHZ_MAX{
+            self.clock_frequency = CLOCK_FREQUENCY_MHZ_MAX;
+            return Err(Error::ClockFrequencyError);
+        }
+        self.clock_frequency = clock_frequency_in_mhz;
+        Ok(()) 
+    }
+
     fn chip_select_high(&mut self) -> Result<(), Error>{
         self.chip_select.set_high().map_err(|_| Error::ChipSelectError)
     }
 
     fn chip_select_low(&mut self) -> Result<(), Error>{
         self.chip_select.set_low().map_err(|_| Error::ChipSelectError)
-    }
-
-    pub fn init(&mut self){
     }
 }
