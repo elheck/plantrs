457d81894b45b88f66ffead01edfa013cff6fd4c
elheck <aron.heck@gmx.de>
2023-09-26 21:21:56 +0200

    undid everything

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,12 @@ use defmt_rtt as _;
 use panic_probe as _;
 use rp_pico::entry;
 
-use embedded_hal::digital::v2::OutputPin;
 use rp_pico::hal::{
-    adc::Adc,
     clocks::{init_clocks_and_plls, Clock},
     pac,
     sio::Sio,
     watchdog::Watchdog,
 };
-
-mod water_pump;
-use crate::water_pump::WaterPump;
-
-mod dht;
-use crate::dht::Dht11;
-
-mod analog_ph_meter;
-use crate::analog_ph_meter::PhProbe;
-
-//mod tmc429;
 
 #[entry]
 fn main() -> ! {
@@ -54,43 +41,7 @@ fn main() -> ! {
         sio.gpio_bank0,
         &mut peripherals.RESETS,
     );
-
-    let mut adc = Adc::new(peripherals.ADC, &mut peripherals.RESETS);
-
-    let mut water_pump = WaterPump::new(
-        pins.gpio7.into_push_pull_output(),
-        pins.gpio4.into_push_pull_output(),
-    );
-    let mut inside_dht = Dht11::new(pins.gpio15.into());
-    let mut outside_dht = Dht11::new(pins.gpio18.into());
-    let mut ph_meter = PhProbe::new(pins.gpio28.into_floating_input());
-    let mut onboard_led = pins.led.into_push_pull_output();
-
-    onboard_led.set_high().unwrap();
-    loop {
-        let measurement_inside = inside_dht.read(&mut delay).unwrap();
-        info!(
-            "Inside Humidity: {}, Temp: {}\n",
-            measurement_inside.temperature, measurement_inside.relative_humidity
-        );
-        let measurement_outside = outside_dht.read(&mut delay).unwrap();
-        info!(
-            "Outside Humidity: {}, Temp: {}\n",
-            measurement_outside.temperature, measurement_outside.relative_humidity
-        );
-        let ph = ph_meter.read(&mut adc);
-        info!("Ph is currently {}\n", ph);
-        match water_pump.turn_on() {
-            Ok(_) => info!("Pump on"),
-            Err(_) => panic!("Could not turn on pump"),
-        };
-        delay.delay_ms(1000);
-        match water_pump.turn_off() {
-            Ok(_) => info!("Pump off"),
-            Err(_) => panic!("Could not turn off pump"),
-        };
-        delay.delay_ms(1000);
-    }
+    loop {}
 }
 
 // End of file
